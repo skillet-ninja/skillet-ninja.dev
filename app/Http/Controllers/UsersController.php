d3f445f6eda71fe8bde076d074ac031bd8225754
Dan Ross <danross826@gmail.com>
2016-10-24 11:40:05 -0500

    I made the UsersController

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if($request->has('search_user')){
+            $users = User::where('name','LIKE','%' . $request['search_user'] . '%')
+            ->orderBy('created_at','desc')
+            ->paginate(10);
+        }else{
+            $users = User::paginate(10);
+        }
+
+        $data['users'] = $users;
+        
+        return view('users.index', $data);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+
+        $rules = array(
+        'name' => 'required|max:100',
+        'email'   => 'required',
+        'password'=>'required',
+        );
+
+        $this->validate($request, $rules);
+
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+        return redirect('/users');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = user::find($id);
+        $data['user'] = $user;
+
+
+        return view('users.show', $data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = user::find($id);
+        $data['user'] = $user;
+        return view('users.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $rules = array(
+        'name' => 'required|max:100',
+        'email'   => 'required',
+        'password'=>'required',
+        );
+
+        $this->validate($request, $rules);
+
+        
+        $user = user::find($id);
+        $user->name = $request->name; 
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return redirect()->action('UsersController@show',$user->id);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return 'delete a specific user';
+    }
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function recipes()
+    {
+            return $this->hasMany('App\Models\Recipe','user_id');
+    }
 }
