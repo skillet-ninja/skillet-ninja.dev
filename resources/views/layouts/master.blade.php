67ba1094d0851310b8315c8e607aea36fb06c908
primalrambler <primal.rambler@gmail.com>
2016-10-26 10:07:00 -0500

    testing/debugging modal view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,7 +40,7 @@
 
 
         {{-- jQuery --}}
-        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc=" crossorigin="anonymous"></script>
+       <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
         {{-- javascript --}}
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
