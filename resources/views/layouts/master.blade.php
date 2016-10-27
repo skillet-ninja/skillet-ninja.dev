efd9c839ee00764b67f4dfaa96523c927b80ae61
primalrambler <primal.rambler@gmail.com>
2016-10-27 12:11:37 -0500

    added js cdn call that was included in a number of partials for the ratings.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -39,6 +39,7 @@
        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
         {{-- javascript --}}
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+       <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 
         @yield('bottom-scripts')
 
