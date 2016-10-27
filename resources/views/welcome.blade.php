3b778b9d09074c3aa08af7f19b9c820c65ef93a6
primalrambler <primal.rambler@gmail.com>
2016-10-27 17:36:09 -0500

    edit user page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         </div>
         <div class="col-md-6">
             <h3>Feel free to browse the available recipes.</h3>
-            <h3>When you find a recipie you want to cook, please login or register then click Skillet!</h3>
+            <h3>When you find a recipe you want to cook, please login or register then click Skillet!</h3>
             <h3>After you login, click the create button to share your recipe with the world!</h3>
             <h3>Happy cooking!</h3>
             <h3 class="welcome-signiture">-Team Skillet Ninja</h3>
