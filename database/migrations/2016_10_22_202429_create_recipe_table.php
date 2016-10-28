546d61d2dc9da7a1ade309d1fa9ea6900972e1e1
Dan Ross <danross826@gmail.com>
2016-10-28 15:24:45 -0500

    started getting votes done, still need to work on recipe controller

--- database/migrations/2016_10_22_202429_create_recipe_table.php
+++ database/migrations/2016_10_22_202429_create_recipe_table.php
@@ -14,17 +14,18 @@ class CreateRecipeTable extends Migration
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->longtext('summary')->nullable();
             $table->longtext('notes')->nullable();
+            $table->integer('vote_score')->default(0);
 
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
 
             $table->integer('servings')->nullable();
-            $table->string('name');
             $table->string('difficulty')->nullable();
             $table->string('image_url')->nullable();
-            $table->integer('overall_time')->nullable();
+            $table->string('overall_time')->nullable();
             $table->timestamps();
         });
     }
