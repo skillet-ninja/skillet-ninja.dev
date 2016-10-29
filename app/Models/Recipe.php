a33a52697ee0d281ae7d4019369bba5a53e1edd1
primalrambler <primal.rambler@gmail.com>
2016-10-29 10:24:33 -0500

    syncing

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -12,10 +12,10 @@ class Recipe extends Model
 
     public function user()
     {
-    	return $this->belongsTo('App\User', 'user_id', 'id');
+        return $this->belongsTo('App\User', 'user_id', 'id');
     }
-	
-	public function steps()
+    
+    public function steps()
     {
         return $this->hasMany('App\Models\Step', 'recipe_id');
     }
@@ -24,21 +24,58 @@ class Recipe extends Model
     {
         return $this->belongsToMany('App\Models\Ingredient')->withPivot('amount')->withTimestamps();
     }
-	
-	public function tags()
+    
+    public function tags()
     {
         return $this->belongsToMany('App\Models\Tag')->withTimestamps();
     }
 
 
     public function getSteps($id) {
-        // return Step::where('recipe_id', $this->id);
+
         return DB::table('steps')->where('recipe_id', $id)->orderBy('created_at', 'asc')->get();
     }
 
-    public static function getSearchTerm($searchTerm)
+    // vote logic begins here
+
+    public function votes()
     {
-        return self::where('name','LIKE','%' . $searchTerm .'%');
+        return $this->hasMany(Vote::class);
+    }
+    
+    public function upvotes()
+    {
+        return $this->votes()->where('vote', '=', 1);
+    }
+    
+    public function downvotes()
+    {
+        return $this->votes()->where('vote', '=', 0);
+    }
+    
+    public function voteScore()
+    {
+        // find total upvotes
+        $upvotes = $this->upvotes()->count();
+        // find total downvotes
+        $downvotes = $this->downvotes()->count();
+        // return upvotes - downvotes
+        return $upvotes - $downvotes;
+    }
+    
+    public function userVote($userId)
+    {
+        return $this->votes()->where('user_id', '=', $userId)->first();
+    }
+
+
+        public static function calculateVoteScore()
+    {
+        $recipes = self::all();
+        foreach ($recipes as $recipe) {
+            $recipe->vote_score = $recipe->voteScore();
+            $recipe->save();
+        }
     }
 
 }
