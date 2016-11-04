5b08a8a85fb5abd62a22e9f353e39cb56a282044
Justin Reich <justinreich.dev@gmail.com>
2016-11-04 18:48:57 -0500

    Made user edit and show pages more mobile responsive

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -20,55 +20,57 @@
     <hr>
 
     <div class="row">
-        <div class="col-md-4">
-            <i class="fa fa-user fa-5x pull-left" aria-hidden="true"></i>
-                <h1>{{ $user->name }}</h1>
-                <h3>{{ $user->email }}</h3><br>
+        <div class="col-md-offset-3 col-md-6 text-center">
+            <i class="fa fa-user fa-5x" aria-hidden="true"></i>
+            <h1 class="usernameStyle">{{ $user->name }}</h1>
+            <h3 class="emailStyle">{{ $user->email }}</h3>
         </div>
-    <div class="col-md-4">
+        <div class="col-md-offset-3 col-md-6">
 
 
-    <form method="POST" action="{{ action('UsersController@update', $user->id) }}">
-        {!! csrf_field() !!}
-        {!! method_field('PUT') !!}
+            <form method="POST" action="{{ action('UsersController@update', $user->id) }}">
+                {!! csrf_field() !!}
+                {!! method_field('PUT') !!}
 
-        <div class="form-group ">
-            <label for="name">Username</label>
-            <input type="text" class="form-control" name="name" value="{{ (old('name') == null) ? $user->name : old('name') }}">
-        </div>
+                <div class="form-group ">
+                    <label for="name">Username</label>
+                    <input type="text" class="form-control" name="name" value="{{ (old('name') == null) ? $user->name : old('name') }}">
+                </div>
 
-        <div class="form-group ">
-            <label for="email">Email address</label>
-            <input type="text" class="form-control" name="email" value="{{ (old('email') == null) ? $user->email : old('email') }}">
-        </div>
+                <div class="form-group ">
+                    <label for="email">Email address</label>
+                    <input type="text" class="form-control" name="email" value="{{ (old('email') == null) ? $user->email : old('email') }}">
+                </div>
 
-        <div class="form-group ">
-            <label for="password">Password</label>
-            <input class="form-control" name="password" value="">
-        </div>
-        <div class="form-group">
-            <label for="password_confirmation">Password Confirmation</label>
-            <input type="password" class="form-control" name="password_confirmation">
-        </div>
+                <div class="form-group ">
+                    <label for="password">Password</label>
+                    <input class="form-control" name="password" value="">
+                </div>
+                <div class="form-group">
+                    <label for="password_confirmation">Password Confirmation</label>
+                    <input type="password" class="form-control" name="password_confirmation">
+                </div>
 
-        <div class="row">
-            <div class="col-xs-6">
-                <form action="{{action('UsersController@destroy', $user->id)}}" method="POST">
-                    {!! csrf_field() !!}
-                    {!! method_field('DELETE')!!}
-                    <button type="submit" class="btn btn-danger pull-left customButtonStyle" >Delete</button>
-                </form>
-            </div>
-            <div class="col-xs-4">
-                <a href="/users/{{ Auth::id() }}" class="btn btn-default customButtonStyle">Cancel</a>
-            </div>
-            <div class="col-xs-2">
-              <button type="submit" class="btn btn-default btn-success customButtonStyle pull-right">Submit</button>
-          </div>
+                <div class="row">
+                    <div class="col-xs-6">
+                        <form action="{{action('UsersController@destroy', $user->id)}}" method="POST">
+                            {!! csrf_field() !!}
+                            {!! method_field('DELETE')!!}
+                            <button type="submit" class="btn btn-danger pull-left customButtonStyle" >Delete</button>
+                        </form>
+                    </div>
+                    <div class="col-xs-4">
+                        <a href="/users/{{ Auth::id() }}" class="btn btn-default customButtonStyle">Cancel</a>
+                    </div>
+                    <div class="col-xs-2">
+                      <button type="submit" class="btn btn-default btn-success customButtonStyle pull-right">Submit</button>
+                  </div>
 
-    </form>
+            </form>
 
     </div> <!-- form -->
 </div>
+
+<br>
 
 @stop
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -36,7 +36,7 @@
 
 
 <div class="row">
-    <div class="col-xs-6 col-xs-offset-3 text-center">
+    <div class="col-xs-12 text-center">
         {!! $recipes->render() !!}
     </div>
 </div>
@@ -60,7 +60,7 @@
 </div>
 
 <div class="row">
-    <div class="col-xs-6 col-xs-offset-3 text-center">
+    <div class="col-xs-12 text-center">
         {!! $upVotedRecipes->render() !!}
     </div>
 </div>
