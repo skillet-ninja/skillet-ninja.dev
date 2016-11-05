4aebd97a713a15f7c34ec5f1e3874d30c53ce4b5
Justin Reich <justinreich.dev@gmail.com>
2016-11-05 08:39:40 -0500

    Fixed logic in VCA steps where last step registered as step 0. Fixed responsive search bar styling. Fixed About page img aspect ratio. Edited about page text to better describe Skillet Ninja.

--- resources/views/users/about.blade.php
+++ resources/views/users/about.blade.php
@@ -8,7 +8,7 @@
         <div class="col-sm-12 col-md-12">
             <div class="caption">
                 <h3>Greetings!</h3>
-                <p>Skillet Ninja is an MVC Web App, and serves as the capstone project for Codeup, a full-stack web developement career accelerator in San Antonio, TX. Our application provides the cooking challenged with a virtual cooking assistant to walk through cooking the various recipes on our site. Members may also post and edit new recipes. Development technologies used include PHP, Laravel 5.1, MySQL, JavaScript, CSS, Twitter Boostrap, annyang.js, Web Speech API, and Adobe Illustrator.</p> 
+                <p>Skillet Ninja is an MVC Web App, and serves as the capstone project for Codeup, a full-stack web developement career accelerator in San Antonio, TX. Our application provides users (the cooking challenged) with a virtual cooking assistant to walk through any recipe on our site. Members may also post and edit new recipes. Development technologies used in building Skillet Ninja include PHP 5.6, Laravel 5.1, MySQL Database, JavaScript, CSS3, HTML5, Twitter Boostrap, annyang.js, Web Speech API, Balsamiq, and Adobe Illustrator.</p> 
                 <p>We hope you enjoy using our site. Happy cooking!</p> 
                 <p>-Team Skillet Ninja</p>
             </div>
