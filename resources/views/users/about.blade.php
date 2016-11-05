8fc1b08db5776717cd2a9e521aeacfa65ca29a4b
Justin Reich <justinreich.dev@gmail.com>
2016-11-05 12:21:47 -0500

    Added animation transitions to all major views and styled pagination in VCA

--- resources/views/users/about.blade.php
+++ resources/views/users/about.blade.php
@@ -2,68 +2,73 @@
 
 @section('content')
 
-    <h1 class="text-center">Team Skillet Ninja</h1>
+    <div class="animated fadeIn">
+        
+        <h1 class="text-center">Team Skillet Ninja</h1>
 
-    <div class="row">
-        <div class="col-sm-12 col-md-12">
-            <div class="caption">
-                <h3>Greetings!</h3>
-                <p>Skillet Ninja is an MVC Web App, and serves as the capstone project for Codeup, a full-stack web developement career accelerator in San Antonio, TX. Our application provides users (the cooking challenged) with a virtual cooking assistant to walk through any recipe on our site. Members may also post and edit new recipes. Development technologies used in building Skillet Ninja include PHP 5.6, Laravel 5.1, MySQL Database, JavaScript, CSS3, HTML5, Twitter Boostrap, annyang.js, Web Speech API, Balsamiq, and Adobe Illustrator.</p> 
-                <p>We hope you enjoy using our site. Happy cooking!</p> 
-                <p>-Team Skillet Ninja</p>
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+                <div class="caption">
+                    <h3>Greetings!</h3>
+                    <p>Skillet Ninja is an MVC Web App, and serves as the capstone project for Codeup, a full-stack web developement career accelerator in San Antonio, TX. Our application provides users (the cooking challenged) with a virtual cooking assistant to walk through any recipe on our site. Members may also post and edit new recipes. Development technologies used in building Skillet Ninja include PHP 5.6, Laravel 5.1, MySQL Database, JavaScript, CSS3, HTML5, Twitter Boostrap, annyang.js, Web Speech API, Balsamiq, and Adobe Illustrator.</p> 
+                    <p>We hope you enjoy using our site. Happy cooking!</p> 
+                    <p>-Team Skillet Ninja</p>
+                </div>
             </div>
         </div>
-    </div>
-    
-    <br>
+        
+        <br>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <div class="thumbnail">
-                <img class="bioPics" src="/assets/img/DanielCarroll.jpg" alt="...">
-                <div class="caption">
-                    <h3>Daniel Carroll</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="thumbnail">
+                    <img class="bioPics" src="/assets/img/DanielCarroll.jpg" alt="...">
+                    <div class="caption">
+                        <h3>Daniel Carroll</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="thumbnail">
+                    <img class="bioPics" src="/assets/img/JustinReich.jpg" alt="...">
+                    <div class="caption">
+                        <h3>Justin Reich</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                <div class="thumbnail">
+                    <img class="bioPics" src="/assets/img/DanielRoss.jpg" alt="...">
+                    <div class="caption">
+                        <h3>Daniel Ross</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <div class="thumbnail">
-                <img class="bioPics" src="/assets/img/JustinReich.jpg" alt="...">
-                <div class="caption">
-                    <h3>Justin Reich</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
-            <div class="thumbnail">
-                <img class="bioPics" src="/assets/img/DanielRoss.jpg" alt="...">
-                <div class="caption">
-                    <h3>Daniel Ross</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    
-                </div>
-            </div>
-        </div>
+        
     </div>
+
     
 
 @stop
